98e723625f3211d965e8bfb2d4eccae92320c90f
Carlos Mario <carlosmariocb@gmail.com>
2026-03-11 01:20:27 -0500

    now the big and medium asteroids split after shooting

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
-from circleshape import CircleShape
-from constants import LINE_WIDTH
 import pygame
+import random
+from circleshape import CircleShape
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,4 +13,21 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
         
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+        first_new_asteroid_vel = self.velocity.rotate(random_angle)
+        second_new_asteroid_vel = self.velocity.rotate(-random_angle)
+        new_asteroids_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        first_new_asteroid = Asteroid(self.position.x, self.position.y, new_asteroids_radius)
+        second_new_asteroid = Asteroid(self.position.x, self.position.y, new_asteroids_radius)
+        
+        first_new_asteroid.velocity = first_new_asteroid_vel * 1.2
+        second_new_asteroid.velocity = second_new_asteroid_vel * 1.2
